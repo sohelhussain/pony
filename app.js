270ae12700872a7f94a3846b3fc47d209cf961eb
sohelhussain <sohelhussaing@gmail.com>
2024-03-29 18:44:50 +0530

    feat: creat locomotive effect by gsap

--- app.js
+++ app.js
@@ -14,12 +14,50 @@ const scroll = () => {
   gsap.ticker.lagSmoothing(0)
 };
 scroll();
+const locoScroll = () => {
+  gsap.registerPlugin(ScrollTrigger);
+
+// Using Locomotive Scroll from Locomotive https://github.com/locomotivemtl/locomotive-scroll
+
+const locoScroll = new LocomotiveScroll({
+  el: document.querySelector("#main"),
+  smooth: true
+});
+// each time Locomotive Scroll updates, tell ScrollTrigger to update too (sync positioning)
+locoScroll.on("scroll", ScrollTrigger.update);
+
+// tell ScrollTrigger to use these proxy methods for the "#main" element since Locomotive Scroll is hijacking things
+ScrollTrigger.scrollerProxy("#main", {
+  scrollTop(value) {
+    return arguments.length ? locoScroll.scrollTo(value, 0, 0) : locoScroll.scroll.instance.scroll.y;
+  }, // we don't have to define a scrollLeft because we're only scrolling vertically.
+  getBoundingClientRect() {
+    return {top: 0, left: 0, width: window.innerWidth, height: window.innerHeight};
+  },
+  // LocomotiveScroll handles things completely differently on mobile devices - it doesn't even transform the container at all! So to get the correct behavior and avoid jitters, we should pin things with position: fixed on mobile. We sense it by checking to see if there's a transform applied to the container (the LocomotiveScroll-controlled element).
+  pinType: document.querySelector("#main").style.transform ? "transform" : "fixed"
+});
+
+
+
+
+
+
+
+// each time the window updates, we should refresh ScrollTrigger and then update LocomotiveScroll. 
+ScrollTrigger.addEventListener("refresh", () => locoScroll.update());
+
+// after everything is set up, refresh() ScrollTrigger and update LocomotiveScroll because padding may have been added for pinning, etc.
+ScrollTrigger.refresh();
+
+}
+// locoScroll();
 
 
 let clutter = "";
 const text = document.querySelector(`.hero-h1`);
 text.textContent.split("").forEach((words) => {
-  clutter += `<span class="spa uppercase text-[37vw] translate-y-[80%] inline-block tilet-font">${words}</span>`;
+  clutter += `<span class="spa text-black uppercase text-[37vw] translate-y-[80%] inline-block tilet-font">${words}</span>`;
 });
 text.innerHTML = clutter;
 
@@ -184,3 +222,24 @@ gsap.to(".spa",{
     scrub: 1
   }
 })
+const time = gsap.timeline({
+  scrollTrigger:{
+    trigger: ".page1",
+    scroller: "body",
+    // markers: true,
+    start: "-60% 0%",
+    end: "100% 100%",
+    scrub: 1
+  }
+});
+time.to(".spark-img",{
+  rotate:5,
+  y:"10%"
+},"box")
+time.to(".let",{
+  rotate:-5,
+  y:"-20%"
+},"box")
+time.to(".lazy",{
+  y:"5%"
+},"box")
